6a16348c8673a57f5b68866be0543e16403da86b
omartins-zs <gabrielmatheus209@gmail.com>
2023-03-29 15:04:34 -0300

    Importando file Songs e Create Play/Pause Music

--- js/script.js
+++ js/script.js
@@ -7,3 +7,25 @@ const currentTime = document.querySelector("#currentTime");
 const duration = document.querySelector("#duration");
 const progressBar = document.querySelector(".progress-bar");
 const progress = document.querySelector(".progress");
+
+import songs from "./js/songs.js";
+
+const textButtonPlay = "<i class='bx bx-caret-right'></i>";
+const textButtonPause = "<i class='bx bx-pause'></i>";
+
+let index = 0;
+
+prevButton.onclick = () => prevNextMusic("prev");
+nextButton.onclick = () => prevNextMusic();
+
+playPauseButton.onclick = () => playPause();
+
+const playPause = () => {
+  if (player.paused) {
+    player.play();
+    playPauseButton.innerHTML = textButtonPause;
+  } else {
+    player.pause();
+    playPauseButton.innerHTML = textButtonPlay;
+  }
+};
