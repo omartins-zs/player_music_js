67542b21a683940626077900ab750d3267d4d409
omartins-zs <gabrielmatheus209@gmail.com>
2023-03-29 15:03:16 -0300

    Criado Array de Export do caminho e nome da musica

---
+++ js/songs.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      src: "./src/music/Dude - Patrick Patrikios.mp3",
+      name: "Dude - Patrick Patrikios",
+    },
+    {
+      src: "./src/music/Girl Before A Mirror - Verified Picasso.mp3",
+      name: "Girl Before A Mirror - Verified Picasso",
+    },
+    {
+      src: "./src/music/Like That - Anno Domini Beats.mp3",
+      name: "Like That - Anno Domini Beats",
+    },
+    {
+      src: "./src/music/Shake - Anno Domini Beats.mp3",
+      name: "Shake - Anno Domini Beats",
+    },
+    {
+      src: "./src/music/Turn Up Burn Up - Diamond Ortiz.mp3",
+      name: "Turn Up Burn Up - Diamond Ortiz",
+    },
+    {
+      src: "./src/music/Yo Picasso - Verified Picasso.mp3",
+      name: "Yo Picasso - Verified Picasso",
+    },
+  ];
